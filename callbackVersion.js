86f8cf610e4eb8f75f03725e7736ff2c0b389f66
sagepoweron <108161754+sagepoweron@users.noreply.github.com>
2025-06-01 23:08:33 -0400

    Updated code

--- callbackVersion.js
+++ callbackVersion.js
@@ -6,70 +6,90 @@
 
 function mowYard(name, callback)
 {
-    console.log(`${name} mowed the yard.`);
-
-    setTimeout(weedEat, 1500, name, callback);
+    setTimeout(() =>
+    {
+        console.log(`${name} mowed the yard.`);
+        callback();
+    }, 2000);
 }
 
 function weedEat(name, callback)
 {
-    if (hasFallenAsleep(10))
+    setTimeout(() =>
     {
-        console.log(`${name} fell asleep after mowing the yard.`);
-        return;
-    }
+        if (hasFallenAsleep(10))
+        {
+            console.log(`${name} fell asleep after mowing the yard.`);
+            return;
+        }
 
-    console.log(`${name} finished using the weed eater.`);
-    setTimeout(trimHedges, 1000, name, callback);
+        console.log(`${name} finished using the weed eater.`);
+        callback();
+    }, 1500);
 }
 
 function trimHedges(name, callback)
 {
-    if (hasFallenAsleep(20))
+    setTimeout(() =>
     {
-        console.log(`${name} fell asleep after weed eating the yard.`);
-        return;
-    }
-
-    console.log(`${name} finished trimming the hedges.`);
+        if (hasFallenAsleep(20))
+        {
+            console.log(`${name} fell asleep after weed eating the yard.`);
+            return;
+        }
+        
+        console.log(`${name} finished trimming the hedges.`);
+        callback();
+    }, 1000);
+    
     setTimeout(collectWood, 2500, name, callback);
 }
 
 function collectWood(name, callback)
 {
-    if (hasFallenAsleep(30))
+    setTimeout(() =>
     {
-        console.log(`${name} fell asleep after trimming the hedges.`);
-        return;
-    }
-
-    console.log(`${name} finished collecting wood.`);
-    setTimeout(waterGarden, 500, name, callback );
+        if (hasFallenAsleep(30))
+        {
+            console.log(`${name} fell asleep after trimming the hedges.`);
+            return;
+        }
+        console.log(`${name} finished collecting wood.`);
+        callback();
+    }, 2500);
 }
 
 function waterGarden(name, callback)
 {
-    if (hasFallenAsleep(40))
+    setTimeout(() =>
     {
-        console.log(`${name} fell asleep after collecting wood.`);
-        return;
-    }
-
-    console.log(`${name} finished watering the garden.`);
-    callback();
+        if (hasFallenAsleep(40))
+        {
+            console.log(`${name} fell asleep after collecting wood.`);
+            return;
+        }
+        console.log(`${name} finished watering the garden.`);
+        callback();
+    }, 500);
+    
 }
 
 
 function doSummerChores(name)
 {
     console.log("started chores");
-    setTimeout
-    (
-        mowYard,
-        2000,
-        name,
-        () => console.log(`${name} finished all their chores!`)
-    );
+
+    mowYard(name, () => {
+        weedEat(name, () => {
+            trimHedges(name, () => {
+                collectWood(name, () => {
+                    waterGarden(name, () => {
+                        console.log(`${name} finished all their chores!`);
+                    })
+                })
+            });
+        });
+    });
 }
 
 function hasFallenAsleep(chance)
